4d9e3ae6a6518071e82f153a000c2add179cf9f1
Finn Schürmann <145472257+fschurma@users.noreply.github.com>
2024-02-12 13:11:06 +0100

    Update Startpage

--- dashboard.py
+++ dashboard.py
@@ -1,82 +1,59 @@
 from dash import Dash, html, dcc, callback_context
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.Div(
-        children=[
+    html.Div([
+        html.Div([
             html.P('File'),
             html.P('Show Demo'),
-            html.P('Add Window'),
-            html.P('Choose Model'),
-            html.P('Import Image', id='import-image'),
-            html.Div(id='upload-container')
-        ],
-        id='top-bar'
-    ),
+            html.P('Add Window', id='p_element'),
+            dcc.Dropdown(['FCN ResNet50', 'FCN ResNet101', 'DeepLabV3 ResNet50','DeepLabV3 ResNet101', 'DeepLabV3 MobileNetV3-Large', 'LR-ASPP MobileNetV3-Large'
+                         ],'FCN ResNet50', id='dropdown'),
+            dcc.Upload(html.P('Choose Model'), accept='.h5'),
+            dcc.Upload(html.P('Import Image'), accept='.jpg, .png, .tiff')
 
-    html.Div([
-        html.Div(className='card', children=[
-            html.P('Choose Model'),
-            html.Img(src='assets/images/neural_network.png', alt='Model Pictogram')
-        ]),
-        html.Div(className='card', children=[
-            html.P('Import Image'),
-            html.Img(src='assets/images/image.png', alt='Image Pictogram')
-            
-        ],
-        id='import-image'
-        ),
-        
-    ], className='row-container')
+        ], id='top-bar'),
+
+        html.Div([
+            html.Div(className='card', children=[
+                html.P('Choose Model'),
+                html.Img(src='assets/images/neural_network.png', alt='Model Pictogram'),
+            ]),
+            html.Div(className='card', children=[
+                html.P('Import Image'),
+                html.Img(src='assets/images/image.png', alt='Image Pictogram', n_clicks=0),      
+            ]),
+        ], className='row-container'),
+
+        html.Div([
+            dcc.Upload(
+                id='upload-model', 
+                children=html.Button('Upload Model'), accept='.h5'
+            ),
+            dcc.Upload(
+                id='upload-image',
+                children=html.Button('Upload Image'), accept='.jpg, .png, .tiff'
+            )
+        ], className='row-container'),
+    ]),
 ])
 
+@app.callback(
+    Output('dropdown', 'style'),
+    [Input('p_element', 'n_clicks')]
+)
+def open_dropdown(n_clicks):
+    if n_clicks is not None and n_clicks > 0:
+        return {'display': 'block'}
+    else:
+        return {'display': 'none'}
+
+# Include CSS file
 app.css.append_css({
     'external_url': 'assets/styles.css'
 })
-
-
-@app.callback(
-    Output('upload-container', 'children'),
-    [Input('import-image', 'n_clicks')]
-)
-def toggle_upload(n_clicks):
-    if n_clicks is None:
-        return []
-    else:
-        return dcc.Upload(
-            id='upload-image',
-            children=html.Div([
-                'Drag and Drop or ',
-                html.A('Select Files')
-            ]),
-            style={
-                'width': '100%',
-                'height': '60px',
-                'lineHeight': '60px',
-                'borderWidth': '1px',
-                'borderStyle': 'dashed',
-                'borderRadius': '5px',
-                'textAlign': 'center',
-                'margin': '10px'
-            },
-            multiple=False
-        )
-
-@app.callback(
-    Output('upload-container', 'children'),
-    [Input('top-bar', 'n_clicks')],
-    prevent_initial_call=True
-)
-def close_upload(n_clicks):
-    ctx = callback_context
-    if not ctx.triggered:
-        return []
-    elif ctx.triggered[0]['prop_id'] != 'top-bar.n_clicks':
-        return []
-    else:
-        return []
 
 if __name__ == '__main__':
     app.run_server(debug=True)
